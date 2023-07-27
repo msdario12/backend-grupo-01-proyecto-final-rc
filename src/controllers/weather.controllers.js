2d1929cc780cc0252de34f464df56642b6740e42
msdario12 <mansilladario94@gmail.com>
2023-07-26 21:14:11 -0300

    configuracion basica de axios para la api de apiWeather

--- src/controllers/weather.controllers.js
+++ src/controllers/weather.controllers.js
@@ -1,9 +1,22 @@
-const getWeatherInfo = (req, res) => {
-	console.log('Weather');
-    const API_KEY = process.env.API_KEY_WEATHER
-	res.json({
-		msg: 'Welcome to weather',
-	});
+const axios = require('axios');
+const { weatherAPI } = require('../api/weatherAPI');
+
+const getWeatherInfo = async (req, res) => {
+	try {
+		const config = {
+			q: '48.8567,2.3508',
+		};
+		const response = await weatherAPI.get(`/current.json`, {
+			params: config,
+		});
+		console.log(response.data);
+		res.json({
+			success: true,
+			data: response.data,
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports = { getWeatherInfo };
