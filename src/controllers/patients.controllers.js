02bf4daf19f6f82183faa3bb0f46110ff9315383
msdario12 <mansilladario94@gmail.com>
2023-07-29 11:40:43 -0300

    se corrige logica para añdir nuevo paciente, de manera de poder agregar una nueva mascota a un cliente existente

--- src/controllers/patients.controllers.js
+++ src/controllers/patients.controllers.js
@@ -5,22 +5,33 @@ const { Patient } = require('../models/patients.models');
 const createNewPatient = async (req, res, next) => {
 	try {
 		const { firstName, lastName, email, phone, name, specie, race } = req.body;
-        
+		console.log(req.body);
 		const foundedUser = await User.findOne({ email: email });
 		if (foundedUser) {
-			res.status(200).json({
+			const newPet = await Pet.create({
+				name,
+				specie,
+				race,
+				client_id: foundedUser._id,
+			});
+
+			foundedUser.pets.push(newPet._id);
+
+			await foundedUser.save();
+
+			const newPatient = await Patient.create({
+				user_id: foundedUser._id,
+				pet_id: newPet._id,
+			});
+			await newPatient.save();
+			console.log(foundedUser);
+			res.status(201).json({
 				success: true,
-				message: 'El email ya está registrado',
+				data: newPatient,
 			});
 			return;
 		}
 		const newUser = await User.create({ firstName, lastName, email, phone });
-		const newPet = await Pet.create({
-			name,
-			specie,
-			race,
-			client_id: newUser._id,
-		});
 
 		newUser.pets.push(newPet._id);
 
