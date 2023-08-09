c8d7f82e26c11beeba417c18ee2a0394a81d6023
msdario12 <mansilladario94@gmail.com>
2023-08-09 18:51:55 -0300

    se cambia cors al nuevo dominio

--- config/cors-options.js
+++ config/cors-options.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-	origin: 'http://localhost:5173',
+	origin: 'https://rolling-vet23.netlify.app',
 	credentials: true,
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowedHeaders: [
