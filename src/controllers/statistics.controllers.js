56b47bd411bd93be218bb6f4491e548eec8ea1a7
msdario12 <mansilladario94@gmail.com>
2023-09-11 09:29:14 -0300

    se arreglan datos de estadisticas en el main dashboard
    Se corrige los datos de los proximos turnos a atenderse (pendientes), se corrige el numero de mascotas atendidas en la semana

--- src/controllers/statistics.controllers.js
+++ src/controllers/statistics.controllers.js
@@ -1,84 +1,89 @@
-const { addDays } = require('date-fns');
+const { addDays, endOfWeek } = require('date-fns');
 const { Turn } = require('../models/turns.models');
 const startOfWeek = require('date-fns/startOfWeek');
 const { Pet } = require('../models/pets.models');
 const { Patient } = require('../models/patients.models');
 
 const getGeneralStatistics = async (req, res, next) => {
-	try {
-		const totalTurns = await Turn.find({}).count();
-		const completedTurns = await Turn.find({
-			status: 'completed',
-		}).count();
-		const pendingTurns = await Turn.find({
-			status: 'pending',
-		}).count();
-		const nextTurns = await Turn.find({
-			status: 'pending',
-		})
-			.sort({
-				date: 1,
-			})
-			.select('date')
-			.populate({
-				path: 'patient_id',
-				strictPopulate: false,
-				select: 'pet_id',
-				populate: {
-					path: 'pet_id',
-					select: 'name specie',
-				},
-				perDocumentLimit: 2,
-			})
-			.limit(2);
-		// obtenemos la semana actual
-		const startDayOfTheWeek = startOfWeek(new Date());
-		console.log(startDayOfTheWeek);
-		const patientsSeenInWeek = await Turn.find({
-			date: {
-				$gt: startDayOfTheWeek,
-				$lt: new Date(),
-			},
-		}).count();
-		// Mascota más habitual
-		const mostCommonSpecie = await Pet.aggregate([
-			{
-				$group: {
-					_id: '$specie',
-					count: {
-						$sum: 1,
-					},
-				},
-			},
-			{
-				$sort: {
-					count: -1,
-				},
-			},
-			{
-				$limit: 1,
-			},
-		]).exec();
-		// total de pacientes
-		const totalRegisteredPatients = await Patient.find({}).count();
+    try {
+        const totalTurns = await Turn.find({}).count();
+        const completedTurns = await Turn.find({
+            status: 'completed',
+        }).count();
+        const pendingTurns = await Turn.find({
+            status: 'pending',
+        }).count();
+        const nextTurns = await Turn.find({
+            status: 'pending',
+            date: {
+                $gt: new Date()
+            },
+        })
+            .sort({
+                date: 1,
+            })
+            .select('date')
+            .populate({
+                path: 'patient_id',
+                strictPopulate: false,
+                select: 'pet_id',
+                populate: {
+                    path: 'pet_id',
+                    select: 'name specie',
+                },
+                perDocumentLimit: 2,
+            })
+            .limit(2);
+        // obtenemos la semana actual
+        const startDayOfTheWeek = startOfWeek(new Date());
+        const endDayOfTheWeek = endOfWeek(new Date())
+        console.log(startDayOfTheWeek);
+        const patientsSeenInWeek = await Turn.find({
+            date: {
+                $gt: startDayOfTheWeek,
+                $lt: endDayOfTheWeek,
+            },
+            status: 'completed'
+        }).count();
+        // Mascota más habitual
+        const mostCommonSpecie = await Pet.aggregate([
+            {
+                $group: {
+                    _id: '$specie',
+                    count: {
+                        $sum: 1,
+                    },
+                },
+            },
+            {
+                $sort: {
+                    count: -1,
+                },
+            },
+            {
+                $limit: 1,
+            },
+        ]).exec();
+        // total de pacientes
+        const totalRegisteredPatients = await Patient.find({}).count();
 
-		const statisticsData = {
-			totalTurns,
-			completedTurns,
-			pendingTurns,
-			nextTurns,
-			patientsSeenInWeek,
-			mostCommonSpecie,
-			totalRegisteredPatients,
-		};
+        const statisticsData = {
+            totalTurns,
+            completedTurns,
+            pendingTurns,
+            nextTurns,
+            patientsSeenInWeek,
+            mostCommonSpecie,
+            totalRegisteredPatients,
+        };
 
-		res.status(200).json({
-			success: true,
-			data: statisticsData,
-		});
-	} catch (error) {
-		next(error);
-	}
+        res.status(200).json({
+            success: true,
+            data: statisticsData,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 module.exports = { getGeneralStatistics };
